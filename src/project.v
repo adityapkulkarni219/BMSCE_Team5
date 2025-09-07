1a9a8fd2b330d85c8330e2e0203ce8332a18970c
adityapkulkarni219 <adityapkulkarni219@gmail.com>
2025-09-07 21:49:55 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,12 +16,50 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    shift_register_design mydesign(
+        .clk (clk),
+        .reset (rst_n),
+        .load (ui_in[0]),
+        .serial_input (ui_in[1]),
+        .direction (ui_in[2]),
+        .parallel_load (ui_in[6:3]),
+        .parallel_out (uo_out[3:0])
+    );
+    
+    assign uo_out[7:4] = 4'b0;
+    assign uio_out[7:0] = 8'b0;
+    assign uio_oe [7:0] = 8'b0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena,1'b0,  uio_in[7:0], uio_out[7:0], uio_oe[7:0], ui_in[7]};
+endmodule
+
+
+module shift_register_design(
+    input clk,
+    input serial_input,
+    input load,
+    input direction,   // 0 = shift right, 1 = shift left
+    input reset,
+    input [3:0] parallel_load,
+    output reg [3:0] parallel_out
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        parallel_out <= 4'b0;
+    end 
+    else if (load) begin
+        // Load all 4 bits directly
+        parallel_out <= parallel_load;
+    end 
+    else begin
+        case (direction)
+            1'b0: parallel_out <= {serial_input, parallel_out[3:1]}; // Shift right
+            1'b1: parallel_out <= {parallel_out[2:0], serial_input}; // Shift left
+            default: parallel_out <= 4'b0;
+        endcase
+    end
+end
 
 endmodule
